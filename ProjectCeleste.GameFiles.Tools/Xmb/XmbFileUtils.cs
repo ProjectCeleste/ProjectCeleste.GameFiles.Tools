831317147f8e2b9b08c18d2f32dd29e014d2e549
Ryddag <projectceleste0@gmail.com>
2019-05-03 16:01:33 +0200

    Update XmbFileUtils.cs

--- ProjectCeleste.GameFiles.Tools/Xmb/XmbFileUtils.cs
+++ ProjectCeleste.GameFiles.Tools/Xmb/XmbFileUtils.cs
@@ -52,5 +52,48 @@ namespace ProjectCeleste.GameFiles.Tools.Xmb
                 }
             }
         }
+
+        public static byte[] XmbToXml(byte[] data)
+        {
+            using (var fileStream = new MemoryStream(data, false))
+            {
+                using (var reader = new BinaryReader(fileStream))
+                {
+                    var head = new string(reader.ReadChars(2));
+                    if (head != "X1")
+                        throw new Exception("Invalid XMB (head does not equal X1)");
+                    reader.ReadInt32();
+                    reader.ReadChars(2);
+                    reader.ReadInt32();
+                    reader.ReadInt32();
+                    //
+                    var elementcount = reader.ReadInt32();
+                    var elementNames = new List<string>();
+                    for (var index = 0; index < elementcount; ++index)
+                    {
+                        var strLength = reader.ReadUInt32();
+                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int)strLength * 2));
+
+                        elementNames.Add(str);
+                    }
+                    //
+                    var attrcount = reader.ReadInt32();
+                    var attrNames = new List<string>();
+                    for (var index = 0; index < attrcount; ++index)
+                    {
+                        var strLength = reader.ReadUInt32();
+                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int)strLength * 2));
+
+                        attrNames.Add(str);
+                    }
+                    //
+                    var xmbElement = XmbElement.DeserializeXmbElement(reader, elementNames, attrNames);
+
+                    var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + xmbElement.ToXml();
+
+                    return Encoding.UTF8.GetBytes(xml);
+                }
+            }
+        }
     }
 }
