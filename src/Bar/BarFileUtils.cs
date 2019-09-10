d85d0fcfab6a8da8a3af9a9107638b335cbe3a6a
Ryddag <projectceleste0@gmail.com>
2019-09-10 16:32:16 +0200

    Update L33TZipUtils.cs

--- src/Bar/BarFileUtils.cs
+++ src/Bar/BarFileUtils.cs
@@ -102,7 +102,7 @@ namespace ProjectCeleste.GameFiles.Tools.Bar
                                     var tempFileName2 =
                                         Path.Combine(Path.GetTempPath(),
                                             $"{Path.GetFileName(barFileInfo.FileName)}-{rnd.Next()}.tmp");
-                                    L33TZipUtils.DoExtractL33TZipFile(tempFileName, tempFileName2);
+                                    L33TZipUtils.ExtractL33TZipFile(tempFileName, tempFileName2);
 
                                     if (File.Exists(tempFileName))
                                         File.Delete(tempFileName);
@@ -140,7 +140,7 @@ namespace ProjectCeleste.GameFiles.Tools.Bar
                                     var tempFileName2 =
                                         Path.Combine(Path.GetTempPath(),
                                             $"{Path.GetFileName(barFileInfo.FileName)}-{rnd.Next()}.tmp");
-                                    L33TZipUtils.DoCreateL33TZipFile(tempFileName, tempFileName2);
+                                    L33TZipUtils.CreateL33TZipFile(tempFileName, tempFileName2);
 
                                     if (File.Exists(tempFileName))
                                         File.Delete(tempFileName);
@@ -179,6 +179,163 @@ namespace ProjectCeleste.GameFiles.Tools.Bar
                 throw new AggregateException(exceptions.ToArray());
         }
 
+        public static void ExtractBarFile(string inputFile, string file, string outputPath, bool convertFile = true)
+        {
+            if (string.IsNullOrWhiteSpace(file))
+                throw new ArgumentNullException(nameof(file), "Value cannot be null or empty.");
+
+            if (!File.Exists(inputFile))
+                throw new FileNotFoundException($"File '{inputFile}' not found!", inputFile);
+
+            if (!outputPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                outputPath = outputPath + Path.DirectorySeparatorChar;
+
+            BarFile barFilesInfo;
+            using (var fileStream = File.Open(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var binReader = new BinaryReader(fileStream))
+                {
+                    //Read Header
+                    binReader.BaseStream.Seek(0, SeekOrigin.Begin); //Seek to header
+                    var barFileHeader = new BarFileHeader(binReader);
+
+                    //Read Files Info
+                    binReader.BaseStream.Seek(barFileHeader.FilesTableOffset, SeekOrigin.Begin); //Seek to file table
+
+                    barFilesInfo = new BarFile(binReader);
+                }
+            }
+
+            var barFileInfo = barFilesInfo.BarFileEntrys.First(
+                key => string.Equals(key.FileName, file, StringComparison.OrdinalIgnoreCase));
+
+            using (var fileStream = File.Open(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var binReader = new BinaryReader(fileStream))
+                {
+                    binReader.BaseStream.Seek(barFileInfo.Offset, SeekOrigin.Begin); //Seek to file
+
+                    var path = Path.Combine(outputPath, barFilesInfo.RootPath,
+                        Path.GetDirectoryName(barFileInfo.FileName) ?? string.Empty);
+
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+
+                    var filePath = Path.Combine(outputPath, barFilesInfo.RootPath, barFileInfo.FileName);
+
+                    //Extract to tmp file
+                    var tempFileName = Path.GetTempFileName();
+                    using (var fileStreamFinal =
+                        File.Open(tempFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        using (var final = new BinaryWriter(fileStreamFinal))
+                        {
+                            var buffer = new byte[4096];
+                            int read;
+                            var totalread = 0L;
+                            while ((read = binReader.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                if (read > barFileInfo.FileSize)
+                                {
+                                    totalread = barFileInfo.FileSize;
+                                    final.Write(buffer, 0, barFileInfo.FileSize);
+                                }
+                                else if (totalread + read <= barFileInfo.FileSize)
+                                {
+                                    totalread += read;
+                                    final.Write(buffer, 0, read);
+                                }
+                                else if (totalread + read > barFileInfo.FileSize)
+                                {
+                                    var leftToRead = barFileInfo.FileSize - totalread;
+                                    totalread = barFileInfo.FileSize;
+                                    final.Write(buffer, 0, Convert.ToInt32(leftToRead));
+                                }
+
+                                if (totalread >= barFileInfo.FileSize)
+                                    break;
+                            }
+                        }
+                    }
+
+                    //Convert file
+                    if (convertFile)
+                    {
+                        if (L33TZipUtils.IsL33TZipFile(tempFileName) &&
+                            !barFileInfo.FileName.EndsWith(".age4scn", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var rnd = new Random(Guid.NewGuid().GetHashCode());
+                            var tempFileName2 =
+                                Path.Combine(Path.GetTempPath(),
+                                    $"{Path.GetFileName(barFileInfo.FileName)}-{rnd.Next()}.tmp");
+                            L33TZipUtils.ExtractL33TZipFile(tempFileName, tempFileName2);
+
+                            if (File.Exists(tempFileName))
+                                File.Delete(tempFileName);
+
+                            tempFileName = tempFileName2;
+                        }
+
+                        if (barFileInfo.FileName.EndsWith(".xmb", StringComparison.OrdinalIgnoreCase))
+                        {
+                            try
+                            {
+                                var rnd = new Random(Guid.NewGuid().GetHashCode());
+                                var tempFileName2 =
+                                    Path.Combine(Path.GetTempPath(),
+                                        $"{Path.GetFileName(barFileInfo.FileName)}-{rnd.Next()}.tmp");
+                                XmbFileUtils.XmbToXml(tempFileName, tempFileName2);
+
+                                if (File.Exists(tempFileName))
+                                    File.Delete(tempFileName);
+
+                                tempFileName = tempFileName2;
+
+                                filePath = filePath.Substring(0, filePath.Length - 4);
+                            }
+                            catch (Exception)
+                            {
+                                //
+                            }
+                        }
+                        else if (barFileInfo.FileName.EndsWith(".age4scn",
+                                     StringComparison.OrdinalIgnoreCase) &&
+                                 !L33TZipUtils.IsL33TZipFile(tempFileName))
+                        {
+                            var rnd = new Random(Guid.NewGuid().GetHashCode());
+                            var tempFileName2 =
+                                Path.Combine(Path.GetTempPath(),
+                                    $"{Path.GetFileName(barFileInfo.FileName)}-{rnd.Next()}.tmp");
+                            L33TZipUtils.CreateL33TZipFile(tempFileName, tempFileName2);
+
+                            if (File.Exists(tempFileName))
+                                File.Delete(tempFileName);
+
+                            tempFileName = tempFileName2;
+                        }
+                    }
+
+                    //Move new file
+                    if (File.Exists(filePath))
+                        File.Delete(filePath);
+
+                    //
+                    File.Move(tempFileName, filePath);
+
+                    //
+                    File.SetCreationTimeUtc(filePath,
+                        new DateTime(barFileInfo.LastWriteTime.Year, barFileInfo.LastWriteTime.Month,
+                            barFileInfo.LastWriteTime.Day, barFileInfo.LastWriteTime.Hour,
+                            barFileInfo.LastWriteTime.Minute, barFileInfo.LastWriteTime.Second));
+
+                    File.SetLastWriteTimeUtc(filePath,
+                        new DateTime(barFileInfo.LastWriteTime.Year, barFileInfo.LastWriteTime.Month,
+                            barFileInfo.LastWriteTime.Day, barFileInfo.LastWriteTime.Hour,
+                            barFileInfo.LastWriteTime.Minute, barFileInfo.LastWriteTime.Second));
+                }
+            }
+        }
+
         public static void CreateBarFile(IReadOnlyCollection<FileInfo> fileInfos, string inputPath,
             string outputFileName, string rootdir, bool ignoreLastWriteTime = true)
         {
@@ -194,7 +351,7 @@ namespace ProjectCeleste.GameFiles.Tools.Bar
                 if (file.FullName.EndsWith(".age4scn", StringComparison.OrdinalIgnoreCase) &&
                     L33TZipUtils.IsL33TZipFile(file.FullName))
                 {
-                    var data = L33TZipUtils.DoExtractL33TZipFile(file.FullName);
+                    var data = L33TZipUtils.ExtractL33TZipFile(file.FullName);
                     File.Delete(file.FullName);
                     File.WriteAllBytes(file.FullName, data);
                     newFilesInfos.Add(new FileInfo(file.FullName));
--- src/L33TZip/L33TZipUtils.cs
+++ src/L33TZip/L33TZipUtils.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Ionic.Zlib;
 
 #endregion
@@ -212,6 +214,97 @@ namespace ProjectCeleste.GameFiles.Tools.L33TZip
             }
         }
 
+        public static async Task DoCreateL33TZipFile(string inputFileName, string outputFileName, CancellationToken ct = default(CancellationToken),
+            IProgress<double> progress = null)
+        {
+            await Task.Run(() =>
+            {
+                if (!File.Exists(inputFileName))
+                    throw new FileNotFoundException($"File '{inputFileName}' not found!", inputFileName);
+
+                try
+                {
+                    if (File.Exists(outputFileName))
+                        File.Delete(outputFileName);
+
+                    var length = new FileInfo(inputFileName).Length;
+                    using (var fileStream = File.Open(inputFileName, FileMode.Open, FileAccess.Read, FileShare.None))
+                    {
+                        using (var fileStreamFinal =
+                            File.Open(outputFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                        {
+                            using (var final = new BinaryWriter(fileStreamFinal))
+                            {
+                                //
+                                final.BaseStream.Position = 0L;
+
+                                //Write L33T Header & File Length
+                                if (length > int.MaxValue)
+                                {
+                                    char[] l33T = { 'l', '6', '6', 't' };
+                                    final.Write(l33T);
+                                    final.Write(length);
+                                }
+                                else
+                                {
+                                    char[] l33T = { 'l', '3', '3', 't' };
+                                    final.Write(l33T);
+                                    final.Write(Convert.ToInt32(length));
+                                }
+
+                                //Write Deflate specification (2 Byte)
+                                final.Write(new byte[] { 0x78, 0x9C });
+
+                                //
+                                using (var a = new DeflateStream(fileStreamFinal, CompressionMode.Compress,
+                                    CompressionLevel.BestCompression))
+                                {
+                                    var buffer = new byte[4096];
+                                    var totalread = 0L;
+                                    int read;
+                                    while ((read = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                                    {
+                                        ct.ThrowIfCancellationRequested();
+
+                                        //
+                                        if (read > length)
+                                        {
+                                            totalread += length;
+                                            a.Write(buffer, 0, Convert.ToInt32(length));
+                                        }
+                                        else if (totalread + read <= length)
+                                        {
+                                            totalread += read;
+                                            a.Write(buffer, 0, read);
+                                        }
+                                        else if (totalread + read > length)
+                                        {
+                                            var leftToRead = length - totalread;
+                                            totalread += leftToRead;
+                                            final.Write(buffer, 0, Convert.ToInt32(leftToRead));
+                                        }
+
+                                        progress?.Report((double)totalread / length);
+
+                                        //
+                                        if (totalread >= length)
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    if (File.Exists(outputFileName))
+                        File.Delete(outputFileName);
+
+                    throw;
+                }
+            }, ct);
+        }
+
         #endregion
 
         #region  Extract
@@ -385,6 +478,98 @@ namespace ProjectCeleste.GameFiles.Tools.L33TZip
                     }
                 }
             }
+        }
+        
+        public static async Task DoExtractL33TZipFile(string fileName, string outputFileName, CancellationToken ct = default(CancellationToken),
+            IProgress<double> progress = null)
+        {
+            await Task.Run(() =>
+            {
+                if (!File.Exists(fileName))
+                    throw new FileNotFoundException($"File '{fileName}' not found!", fileName);
+
+                try
+                {
+                    if (File.Exists(outputFileName))
+                        File.Delete(outputFileName);
+
+                    using (var fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        using (var reader = new BinaryReader(fileStream))
+                        {
+                            //Header && Length
+                            var head = new string(reader.ReadChars(4));
+                            long length;
+                            switch (head.ToLower())
+                            {
+                                case "l33t":
+                                    length = reader.ReadInt32();
+                                    //Skip deflate specification (2 Byte)
+                                    reader.BaseStream.Position = 10L;
+                                    break;
+                                case "l66t":
+                                    length = reader.ReadInt64();
+                                    //Skip deflate specification (2 Byte)
+                                    reader.BaseStream.Position = 14L;
+                                    break;
+                                default:
+                                    throw new FileLoadException($"'l33t' header not found, file: '{fileName}'");
+                            }
+
+                            ct.ThrowIfCancellationRequested();
+                            //
+                            using (var a = new DeflateStream(reader.BaseStream, CompressionMode.Decompress))
+                            {
+                                using (var fileStreamFinal =
+                                    File.Open(outputFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    using (var final = new BinaryWriter(fileStreamFinal))
+                                    {
+                                        var buffer = new byte[4096];
+                                        int read;
+                                        var totalread = 0L;
+                                        while ((read = a.Read(buffer, 0, buffer.Length)) > 0)
+                                        {
+                                            ct.ThrowIfCancellationRequested();
+
+                                            //
+                                            if (read > length)
+                                            {
+                                                totalread += length;
+                                                final.Write(buffer, 0, (int)length);
+                                            }
+                                            else if (totalread + read <= length)
+                                            {
+                                                totalread += read;
+                                                final.Write(buffer, 0, read);
+                                            }
+                                            else if (totalread + read > length)
+                                            {
+                                                var leftToRead = length - totalread;
+                                                totalread += leftToRead;
+                                                final.Write(buffer, 0, (int)leftToRead);
+                                            }
+
+                                            progress?.Report((double)totalread / length);
+
+                                            //
+                                            if (totalread >= length)
+                                                break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    if (File.Exists(outputFileName))
+                        File.Delete(outputFileName);
+
+                    throw;
+                }
+            }, ct);
         }
 
         #endregion
