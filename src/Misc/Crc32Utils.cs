96eabe6424840227b6e0b1d9fb15654deb17a2c9
Ryddag <projectceleste0@gmail.com>
2019-09-10 16:50:24 +0200

    Add Crc32Utils.cs

---
+++ src/Misc/Crc32Utils.cs
@@ -0,0 +1,76 @@
+﻿#region Using directives
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Force.Crc32;
+
+#endregion
+
+namespace ProjectCeleste.GameFiles.Tools.Misc
+{
+    public static class Crc32Utils
+    {
+        public static uint GetCrc32FromFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException($"File '{fileName}' not found!", fileName);
+
+            using (var fs = File.OpenRead(fileName))
+            {
+                var result = 0u;
+                var buffer = new byte[4096];
+                int read;
+                var totalread = 0L;
+                var length = fs.Length;
+                while ((read = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    totalread += read;
+
+                    result = Crc32Algorithm.Append(result, buffer, 0, read);
+
+                    if (totalread >= length)
+                        break;
+                }
+
+                return result;
+            }
+        }
+
+        public static async Task<uint> DoGetCrc32FromFile(string fileName, CancellationToken ct = default(CancellationToken),
+            IProgress<double> progress = null)
+        {
+            return await Task.Run(() =>
+            {
+                if (!File.Exists(fileName))
+                    throw new FileNotFoundException($"File '{fileName}' not found!", fileName);
+
+                using (var fs = File.OpenRead(fileName))
+                {
+                    var result = 0u;
+                    var buffer = new byte[4096];
+                    int read;
+                    var totalread = 0L;
+                    var length = fs.Length;
+                    while ((read = fs.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        //
+                        ct.ThrowIfCancellationRequested();
+
+                        totalread += read;
+
+                        result = Crc32Algorithm.Append(result, buffer, 0, read);
+
+                        progress?.Report((double) totalread / length);
+
+                        if (totalread >= length)
+                            break;
+                    }
+
+                    return result;
+                }
+            }, ct);
+        }
+    }
+}
