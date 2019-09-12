4bf8bde385abc353f6dc56aae322a1050eac9b34
Ryddag <projectceleste0@gmail.com>
2019-09-12 14:19:08 +0200

    Update XmbFileUtils.cs

--- src/Xmb/XmbFileUtils.cs
+++ src/Xmb/XmbFileUtils.cs
@@ -13,43 +13,10 @@ namespace ProjectCeleste.GameFiles.Tools.Xmb
     {
         public static void XmbToXml(string inputFileName, string outputFileName)
         {
-            using (var fileStream = File.Open(inputFileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            using (var fileStream = File.Open(inputFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
-                using (var reader = new BinaryReader(fileStream))
-                {
-                    var head = new string(reader.ReadChars(2));
-                    if (head != "X1")
-                        throw new Exception("Invalid XMB (head does not equal X1)");
-                    reader.ReadInt32();
-                    reader.ReadChars(2);
-                    reader.ReadInt32();
-                    reader.ReadInt32();
-                    //
-                    var elementcount = reader.ReadInt32();
-                    var elementNames = new List<string>();
-                    for (var index = 0; index < elementcount; ++index)
-                    {
-                        var strLength = reader.ReadUInt32();
-                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int) strLength * 2));
-
-                        elementNames.Add(str);
-                    }
-                    //
-                    var attrcount = reader.ReadInt32();
-                    var attrNames = new List<string>();
-                    for (var index = 0; index < attrcount; ++index)
-                    {
-                        var strLength = reader.ReadUInt32();
-                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int) strLength * 2));
-
-                        attrNames.Add(str);
-                    }
-                    //
-                    var xmbElement = XmbElement.DeserializeXmbElement(reader, elementNames, attrNames);
-
-                    var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + xmbElement.ToXml();
-                    File.WriteAllText(outputFileName, xml, Encoding.UTF8);
-                }
+                var result = XmbToXml(fileStream);
+                File.WriteAllText(outputFileName, result, Encoding.UTF8);
             }
         }
 
@@ -57,42 +24,48 @@ namespace ProjectCeleste.GameFiles.Tools.Xmb
         {
             using (var fileStream = new MemoryStream(data, false))
             {
-                using (var reader = new BinaryReader(fileStream))
+                var result = XmbToXml(fileStream);
+                return Encoding.UTF8.GetBytes(result);
+            }
+        }
+        
+        private static string XmbToXml(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                var head = new string(reader.ReadChars(2));
+                if (head != "X1")
+                    throw new Exception("Invalid XMB (head does not equal X1)");
+                reader.ReadInt32();
+                reader.ReadChars(2);
+                reader.ReadInt32();
+                reader.ReadInt32();
+                //
+                var elementcount = reader.ReadInt32();
+                var elementNames = new List<string>();
+                for (var index = 0; index < elementcount; ++index)
                 {
-                    var head = new string(reader.ReadChars(2));
-                    if (head != "X1")
-                        throw new Exception("Invalid XMB (head does not equal X1)");
-                    reader.ReadInt32();
-                    reader.ReadChars(2);
-                    reader.ReadInt32();
-                    reader.ReadInt32();
-                    //
-                    var elementcount = reader.ReadInt32();
-                    var elementNames = new List<string>();
-                    for (var index = 0; index < elementcount; ++index)
-                    {
-                        var strLength = reader.ReadUInt32();
-                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int)strLength * 2));
+                    var strLength = reader.ReadUInt32();
+                    var str = Encoding.Unicode.GetString(reader.ReadBytes((int) strLength * 2));
 
-                        elementNames.Add(str);
-                    }
-                    //
-                    var attrcount = reader.ReadInt32();
-                    var attrNames = new List<string>();
-                    for (var index = 0; index < attrcount; ++index)
-                    {
-                        var strLength = reader.ReadUInt32();
-                        var str = Encoding.Unicode.GetString(reader.ReadBytes((int)strLength * 2));
-
-                        attrNames.Add(str);
-                    }
-                    //
-                    var xmbElement = XmbElement.DeserializeXmbElement(reader, elementNames, attrNames);
-
-                    var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + xmbElement.ToXml();
-
-                    return Encoding.UTF8.GetBytes(xml);
+                    elementNames.Add(str);
                 }
+                //
+                var attrcount = reader.ReadInt32();
+                var attrNames = new List<string>();
+                for (var index = 0; index < attrcount; ++index)
+                {
+                    var strLength = reader.ReadUInt32();
+                    var str = Encoding.Unicode.GetString(reader.ReadBytes((int) strLength * 2));
+
+                    attrNames.Add(str);
+                }
+                //
+                var xmbElement = XmbElement.DeserializeXmbElement(reader, elementNames, attrNames);
+
+                var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + xmbElement.ToXml();
+
+                return xml;
             }
         }
     }
